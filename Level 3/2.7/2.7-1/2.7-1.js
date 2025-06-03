b3a2df3831b91a4610a3dec5f2e082b1b99eafcc
NikitaPutzavr <151025166+NikitaPutzavr@users.noreply.github.com>
2025-06-03 17:24:53 +0300

    modifed

--- Level 3/2.7/2.7-1/2.7-1.js
+++ Level 3/2.7/2.7-1/2.7-1.js
@@ -1,3 +1,13 @@
 // #3 / 7.1 : Дана строка со словами.
 // Отсортируйте слова в алфавитном порядке.
 
+let isText = "This text about, my dog"
+
+function sortString(str) {
+
+    let sortList = str.split(" ").sort((a,b) => a.localeCompare(b))
+    console.log(sortList);
+    
+}
+
+sortString(isText)
