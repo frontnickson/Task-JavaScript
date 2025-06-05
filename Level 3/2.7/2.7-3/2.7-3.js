5f4687e89b20b68ea1ceae2b9c020eaeb0cb07f5
NikitaPutzavr <151025166+NikitaPutzavr@users.noreply.github.com>
2025-06-05 12:29:35 +0300

    modifed

--- Level 3/2.7/2.7-3/2.7-3.js
+++ Level 3/2.7/2.7-3/2.7-3.js
@@ -1,1 +1,25 @@
-// #3 / 7.3 : Даны два числа. Получите массив общих делителей этих чисел.
+// #3 / 7.3 : Даны два числа.
+// Получите массив общих делителей этих чисел.
+
+let isFirstNum = 15;
+let isSecondNum = 21;
+
+function getNums(f, s) {
+
+    let isFirstArr = [];
+    let isSecondArr = [];
+    let result = [];
+
+    for (let i = 1; i <= f; i++) f % i === 0 && isFirstArr.push(i);
+    for (let i = 1; i <= s; i++) s % i === 0 && isSecondArr.push(i);
+
+    for (let i = 0; i < isFirstArr.length; i++) {
+        for (let j = 0; j < isSecondArr.length; j++) {
+            if (isFirstArr[i] === isSecondArr[j]) result.push(isFirstArr[i]);
+        }
+    }
+
+    console.log(result);
+}
+
+getNums(isFirstNum, isSecondNum)
