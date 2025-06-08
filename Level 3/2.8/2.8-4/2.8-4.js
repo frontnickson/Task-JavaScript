617920c1b844db191a0bdc30abdb9e51f0c57993
NikitaPutzavr <151025166+NikitaPutzavr@users.noreply.github.com>
2025-06-08 18:19:45 +0300

    modifed

--- Level 3/2.8/2.8-4/2.8-4.js
+++ Level 3/2.8/2.8-4/2.8-4.js
@@ -1,2 +1,25 @@
 // #3 / 8.4 : Дана строка в формате: 'camelCase'
 // Преобразуйте ее в формат: 'snake_case'
+
+const isString = "oneTwoThreeFourFiveSix"
+
+const getSnakeCase = (string) => {
+
+    let splitText = string.split("")
+    let result = []
+
+    for (let i = 0; i < splitText.length; i++) {
+        if (splitText[i] === splitText[i].toUpperCase()) {
+            result.push("_", splitText[i].toLowerCase())
+        } else {
+            result.push(splitText[i])
+        }
+    }
+
+    console.log(result.join(""));
+
+
+}
+
+
+getSnakeCase(isString)
--- Level 3/2.8/2.8-5/2.8-5.js
+++ Level 3/2.8/2.8-5/2.8-5.js
@@ -5,3 +5,17 @@
 //     [1, 2, 3],
 //     [1, 2, 3],
 // ]
+
+function getArr(){
+
+    let resultArr = [];
+
+    for(let i = 1; i < 5; i++){
+        resultArr.push([1,2,3])
+    }
+
+    console.log(resultArr);
+
+}
+
+getArr()
