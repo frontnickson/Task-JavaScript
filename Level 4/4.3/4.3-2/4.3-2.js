8c63139dc2f5b4892b423d3dbf1ecbb210539aee
NikitaPutzavr <151025166+NikitaPutzavr@users.noreply.github.com>
2025-06-17 17:03:16 +0300

    modifed

--- Level 4/4.3/4.3-2/4.3-2.js
+++ Level 4/4.3/4.3-2/4.3-2.js
@@ -1,10 +1,19 @@
 // #4 / 3.2 : Сделайте функцию, которая параметром будет принимать массив и удалять
 // из него все дубли, которые встречаются больше трех раз.
 
-let isArrNum = [1, 1, 2, 2, 3, 4, 5, 6, 7, 8, 9, 9, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+let isArrNum = [1, 1, 2, 2, 3, 4, 5, 6, 7, 8, 9, 9, 9, 10, 11, 12, 13, 14, 15, 16, 14, 14, 19, 20];
 
 const deleteDublicates = (arr) => {
-    console.log(arr);
+
+    let result = [];
+
+    for (let i = 1; i < arr.length - 1; i++) {
+        let nums = arr.filter((element) => element === i)
+        result.push(nums)
+    }
+
+    return result.filter((element) => element.length <= 2).flat()
+
 }
 
 deleteDublicates(isArrNum);
--- Level 4/4.3/4.3-3/4.3-3.js
+++ Level 4/4.3/4.3-3/4.3-3.js
@@ -1,1 +1,20 @@
-// #4 / 3.3 : Сделайте функцию, которая параметром будет принимать массив и удалять из него одинаковые, рядом стоящие элементы.
+// #4 / 3.3 : Сделайте функцию, которая параметром будет принимать массив
+// и удалять из него одинаковые, рядом стоящие элементы.
+
+let isArrNum = [1, 1, 2, 2, 3, 4, 5, 6, 7, 8, 9, 9, 9, 10, 11, 12, 13, 14, 12, 16, 14, 14, 19, 20];
+
+const deleteDublicates = (arr) => {
+
+    let result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === arr[i + 1]) result.push("" + arr[i])
+        else result.push(arr[i])
+    }
+
+    console.log(result.filter((element) => element !== String(element)));
+
+
+}
+
+deleteDublicates(isArrNum)
