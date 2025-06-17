080223cf59dcb1ce77b3dba70af1d75ecda517db
NikitaPutzavr <151025166+NikitaPutzavr@users.noreply.github.com>
2025-06-17 13:07:35 +0300

    modifed

--- Level 4/4.2/4.2-3/4.2-3.js
+++ Level 4/4.2/4.2-3/4.2-3.js
@@ -1,1 +1,14 @@
-// #4 / 2.3 : Сделайте функцию, которая будет возвращать сколько дней прошло или осталось до заданной даты в году, в зависимости от того, была уже эта дата или нет.
+// #4 / 2.3 : Сделайте функцию, которая будет возвращать сколько
+// дней прошло или осталось до заданной даты в году,
+// в зависимости от того, была уже эта дата или нет.
+
+function getDate(day) {
+
+    const currentDate = new Date();
+    const targetDate = new Date("2025-06-19");
+    const diffInMs = targetDate - currentDate;
+    const diffInDays = Math.floor(diffInMs / (1000 * 60 * 60 * 24));
+    console.log(diffInDays);
+}
+
+getDate();
